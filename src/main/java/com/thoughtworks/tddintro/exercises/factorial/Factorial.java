588f73a5c6b9b83ad8586ca1ba394963ffd4b41e
kizerxl <changoof@gmail.com>
2018-05-09 00:28:29 -0400

    Make Factorial tests pass

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,20 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException {
+        if(i < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        if(i == 0) {
+            return 1;
+        }
+
+        int result = 1;
+        for(int num = i; num > 0; num--) {
+            result *= num;
+        }
+
+        return result;
     }
 }
